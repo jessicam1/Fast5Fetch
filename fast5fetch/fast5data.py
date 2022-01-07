7f2d0f0a2946750d61f7b97c230d03bf010d8ea9
Emmanouil "Manolis" Maragkakis <emmanouil.maragkakis@nih.gov>
2022-01-07 17:54:43 -0600

    fast5data: second attempt to parralize generator

--- fast5fetch/fast5data.py
+++ fast5fetch/fast5data.py
@@ -125,6 +125,45 @@ def worker(input, output, label, window):
 
 class parallel_xy_generator():
 
+    def __init__(self, files, label, window, shuffle=True, par=1):
+        self.files = files
+        self.label = label
+        self.window = window
+        self.shuffle = shuffle
+        self.par = par
+
+        self.pool = mp.Pool(self.par)
+        self.it = self.pool.imap_unordered(self.worker, self.files)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.it)
+
+    def worker(self, fast5):
+        """ Processes a fast5 with single read """
+
+        with get_fast5_file(fast5, mode='r') as f5:
+            for read in f5.get_reads():
+                norm_sample = process_fast5_read(read, self.window)
+                x = norm_sample.reshape((norm_sample.shape[0], 1))
+                y = np.array(self.label)
+                return(x, y)
+
+    #  https://stackoverflow.com/questions/25382455/
+    def __getstate__(self):
+        self_dict = self.__dict__.copy()
+        del self_dict['pool']
+        del self_dict['it']
+        return self_dict
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+
+class parallel_xy_generator2():
+
     # def __init__(self, files, label, window, shuffle=True, par=1):
     #     self.files = files
     #     self.label = label
@@ -155,34 +194,27 @@ class parallel_xy_generator():
         self.window = window
         self.shuffle = shuffle
         self.par = par
+        self.done = False
 
-        # Create queues
+        # Create queue for workers to put results
         m = mp.Manager()
-        self.done_q = m.Queue()
+        self.results = m.Queue()
 
+        # Create a pool of workers and submit jobs
         self.pool = mp.Pool(self.par)
-
-        # def kill_pool(err_msg):
-        #     print(err_msg)
-        #     pool.terminate()
-
         self.it = self.pool.imap_unordered(self.worker, self.files)
 
-        # for f in self.files:
-        #     pool.apply_async(self.worker,
-        #                      kwds=dict(self=self, fast5=f, out_q=self.done_q),
-        #                      error_callback=kill_pool)
-
-        # self.pool.close()
-        # self.pool.join()
-        # m.shutdown()
+        self.pool.close()
+        self.pool.join()
+        self.done = True
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        # return self.done_q.get()
-        return next(self.it)
+        if self.results.empty() and self.done:
+            raise StopIteration
+        return self.results.get()
 
     def worker(self, fast5):
         """ Processes fast5 files and puts individual reads in queue"""
@@ -192,11 +224,9 @@ class parallel_xy_generator():
                 norm_sample = process_fast5_read(read, self.window)
                 x = norm_sample.reshape((norm_sample.shape[0], 1))
                 y = np.array(self.label)
-                return((x,y))
-        #         self.done_q.put((x, y))
-        # return None
+                self.results.put((x, y))
 
-    #  https://stackoverflow.com/questions/25382455/python-notimplementederror-pool-objects-cannot-be-passed-between-processes
+    #  https://stackoverflow.com/questions/25382455/
     def __getstate__(self):
         self_dict = self.__dict__.copy()
         del self_dict['pool']
@@ -205,118 +235,3 @@ class parallel_xy_generator():
 
     def __setstate__(self, state):
         self.__dict__.update(state)
-
-
-# def parallel_xy_generator(files, label, window, shuffle=True, par=1):
-#     """ generates data-label pairs """
-#
-#     if shuffle:
-#         random.shuffle(files)
-#
-#     # Create queues
-#     task_q = Queue()
-#     done_q = Queue()
-#
-#     # Submit tasks:
-#     for f in files:
-#         task_q.put(f)
-#
-#     # Start worker processes
-#     for i in range(par):
-#         Process(target=worker, args=(task_q, done_q, label, window)).start()
-#
-#     # Tell child processes to stop when all tasks are done
-#     for i in range(par):
-#         task_q.put('STOP')
-#
-#     for res in iter(done_q.get):
-#         yield res
-    #
-    #
-    #
-    # for xy in results:
-    #     sample_file = file_list[i]
-    #     with get_fast5_file(sample_file, mode='r') as f5:
-    #         for read in f5.get_reads():
-    #             norm_sample = process_fast5_read(read, window)
-    #             x = norm_sample.reshape((norm_sample.shape[0], 1))
-    #             y = np.array(label)
-    #             yield (x, y)        
-
-# def data_from_csv_generator(csv_file):
-#     reader = csv.reader(csv_file)
-#     for line in reader:
-#         wholeline = 
-
-# class TrainValSplit:
-#     def __init__(self, file_dirs):
-#         self.file_dirs = file_dirs
-#         self.train_val_split() #file_dirs, train_reads, val_reads)
-#
-#     def train_val_split(self):
-#         train_list = []
-#         val_list = []
-#         for directory in self.file_dirs:
-#             path = Path(directory)
-#             for afile in path.rglob("*.fast5"):
-#                 rand_num = random.random()
-#                 if rand_num < 0.1:
-#                     val_list.append(str(afile))
-#                 else:
-#                     train_list.append(str(afile))
-#         random.shuffle(train_list)
-#         random.shuffle(val_list)
-#         return train_list, val_list
-#
-# class SampleGeneratorFromFiles:
-#     def __init__(self, file_list, label, shuffle=True):
-#         self.file_list = file_list
-#         self.label = label
-#         self.shuffle = shuffle
-#
-#     # def __iter__(self):
-#     #     return self
-#
-#     def data_generation(self): #__next__(self): #, file_list, label, numreads, shuffle):
-#         window = 8000
-#         if self.shuffle==True:
-#             random.shuffle(self.file_list)
-#         for i in range(len(self.file_list)):
-#             sample_file = self.file_list[i]
-#             with get_fast5_file(sample_file, mode='r') as f5:
-#                 for read in f5.get_reads():
-#                     whole_sample = np.asarray(read.get_raw_data(scale=True))
-#                     start_col = random.randint(1000, (len(whole_sample)-window))
-#                     sample = whole_sample[start_col:start_col+window:1]
-#                     norm_sample = stats.zscore(sample) # .asarray()
-#                     # sig_output = norm_sample.reshape((sample_array.shape[0], 1))
-#                     # x = tf.convert_to_tensor(sig_output, dtype=tf.float32)
-#                     x = norm_sample.reshape((norm_sample.shape[0], 1))
-#                     # label_output = np.array(self.label)
-#                     # y = tf.convert_to_tensor(label_output, dtype=tf.int16)
-#                     y = np.array(self.label)
-#                     # if i == self.numreads:
-#                     #     break
-#                     yield (x, y)                
-#
-# class BatchGenerator:
-#     """
-#     Spitting out batches of positives and negatives at the proper ratio
-#     """
-#     def __init__(self, pos_files, neg_files, window=8000, batchsize=32, ratio=0.5, shuffle=True):
-#         self.pos_gen = SampleGeneratorFromFiles(pos_files, window, shuffle, 1)
-#         self.neg_gen = SampleGeneratorFromFiles(neg_files, window, shuffle, 0)
-#         self.window = window
-#         self.batchsize = batchsize
-#         self.ratio = ratio
-#
-#     def __next__(self):
-#         batch = []
-#         for i in range(0, self.batchsize):
-#             random_num = random.random()
-#             if random_num < self.ratio:
-#                 batch += [next(self.pos_gen)]
-#             else:
-#                 batch += [next(self.neg_gen)]
-#         yield batch
-#
